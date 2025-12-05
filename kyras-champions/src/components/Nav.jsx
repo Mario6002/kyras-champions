fbbb6635493550ddbbdb3d40c16479f13bd9354f
Mario6002 <benavidezmario084@gmail.com>
2025-12-04 23:15:48 -0500

    Update Nav.jsx

--- kyras-champions/src/components/Nav.jsx
+++ kyras-champions/src/components/Nav.jsx
@@ -1,1 +1,14 @@
-export default function Nav(){ return <nav></nav>; }
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Nav() {
+  return (
+    <nav className="nav" aria-label="Main navigation">
+      <ul className="nav-list">
+        <li><Link to="/" title="Home">Home</Link></li>
+        <li><Link to="/team" title="Team">Team</Link></li>
+        <li><Link to="/contact" title="Contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
