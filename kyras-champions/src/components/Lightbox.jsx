265cc3fcf78c9920972e2919fcf64975ad637ed2
Mario6002 <benavidezmario084@gmail.com>
2025-12-04 23:16:29 -0500

    Update Lightbox.jsx

--- kyras-champions/src/components/Lightbox.jsx
+++ kyras-champions/src/components/Lightbox.jsx
@@ -1,1 +1,13 @@
-export default function Lightbox(){ return <div></div>; }
+import React from "react";
+
+/*
+ Usage:
+ <a href="#img1"><img src="..." alt="..." /></a>
+ <div id="img1" className="lightbox">
+   <a className="close" href="#!">×</a>
+   <img src="..." alt="" />
+ </div>
+*/
+export default function Lightbox() {
+  return null; // component is demonstrated via HTML in pages where needed
+}
